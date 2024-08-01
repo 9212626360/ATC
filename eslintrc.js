7883a2f1911c9410dce837a8768549c741600140
SAHIL GUPTA <Lucky1000.friend@gmail.com>
2024-08-01 14:51:09 +0530

    Update eslintrc.js

--- eslintrc.js
+++ eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
-    extends: ['react-app', 'react-app/jest'],
-    plugins: ['react', 'jsx-a11y'],
-    rules: {
-      'jsx-a11y/anchor-is-valid': 'off', // Disable the rule during deployment
-      // other rules
-    },
-    overrides: [
-      {
-        files: ['*.js', '*.jsx'],
-        env: {
-          ci: process.env.CI === 'true' ? false : true,
-        },
+  extends: ['react-app', 'react-app/jest'],
+  plugins: ['react', 'jsx-a11y'],
+  rules: {
+    'jsx-a11y/anchor-is-valid': 'off', // Disable the rule during deployment
+    // other rules
+  },
+  overrides: [
+    {
+      files: ['*.js', '*.jsx'],
+      env: {
+        "browser": true,
+        "node": true,
+        ci: process.env.CI === 'true' ? false : true,
       },
-    ],
-  };
-  
+    },
+  ],
+};
