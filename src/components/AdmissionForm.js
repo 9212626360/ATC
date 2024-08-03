85b36a152e46f9ae03c0e7652af3d52a9bf4df77
SAHIL GUPTA <Lucky1000.friend@gmail.com>
2024-08-03 12:46:09 +0530

    done

--- src/components/AdmissionForm.js
+++ src/components/AdmissionForm.js
@@ -1,184 +1,131 @@
-import React, { useState } from 'react';
-import './AdmissionForm.css';
+import React from "react";
+import "./AdmissionForm.css"; // Assuming you will use CSS for additional styling
 
 const AdmissionForm = () => {
-  const [formData, setFormData] = useState({
-    formNo: '',
-    registrationNo: '',
-    courseName: '',
-    courseDuration: '',
-    timeSlot: '',
-    fullName: '',
-    dateOfBirth: '',
-    sex: '',
-    fatherName: '',
-    motherName: '',
-    occupation: '',
-    monthlyIncome: '',
-    postalAddress: '',
-    permanentAddress: '',
-    phoneNo: '',
-    nationality: '',
-    maritalStatus: '',
-    examPassed: '',
-    boardUniversity: '',
-    yearOfPassing: '',
-    division: '',
-    date: '',
-    place: '',
-    guardianSignature: '',
-    applicantSignature: ''
-  });
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle form submission
-    console.log(formData);
-  };
-
   return (
-    <div className="admission-form">
-      <h1>Admission Form</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="row">
+    <div className="container">
+      <h1 className="text-center">AGGARWAL TYPING COLLEGE</h1>
+      <h2 className="text-center text-white bg-blue">Admission Form</h2>
+      <form>
+        <fieldset>
+          <legend><b>All Information</b></legend>
           <div className="form-group">
             <label>Form No:</label>
-            <input type="text" name="formNo" value={formData.formNo} onChange={handleChange} />
+            <input type="text" name="formNo" className="form-control" />
           </div>
           <div className="form-group">
             <label>Registration No:</label>
-            <input type="text" name="registrationNo" value={formData.registrationNo} onChange={handleChange} />
+            <input type="number" name="regNo" className="form-control" />
           </div>
-        </div>
-
-        <div className="row">
           <div className="form-group">
             <label>Course Name:</label>
-            <input type="text" name="courseName" value={formData.courseName} onChange={handleChange} />
+            <input type="text" name="courseName" className="form-control" />
           </div>
           <div className="form-group">
             <label>Course Duration:</label>
-            <input type="text" name="courseDuration" value={formData.courseDuration} onChange={handleChange} />
+            <select name="courseDuration" className="form-control">
+              <option value="Select">Select</option>
+              <option value="3 Months">3 Months</option>
+              <option value="6 Months">6 Months</option>
+              <option value="1 Year">1 Year</option>
+            </select>
           </div>
           <div className="form-group">
             <label>Time Slot:</label>
-            <input type="text" name="timeSlot" value={formData.timeSlot} onChange={handleChange} />
-          </div>
-        </div>
-
-        <div className="form-group">
-          <label>Full Name (In Block Letters):</label>
-          <input type="text" name="fullName" value={formData.fullName} onChange={handleChange} />
-        </div>
-
-        <div className="row">
-          <div className="form-group">
-            <label>Date of Birth:</label>
-            <input type="date" name="dateOfBirth" value={formData.dateOfBirth} onChange={handleChange} />
+            <select name="timeSlot" className="form-control">
+              <option value="Select">Select</option>
+              <option value="Morning">Morning</option>
+              <option value="Evening">Evening</option>
+            </select>
           </div>
           <div className="form-group">
-            <label>Sex (Male/Female):</label>
-            <input type="text" name="sex" value={formData.sex} onChange={handleChange} />
+            <label>Full Name:</label>
+            <input type="text" name="fullName" className="form-control" />
           </div>
-        </div>
-
-        <div className="row">
           <div className="form-group">
-            <label>Father's Name/Husband Name:</label>
-            <input type="text" name="fatherName" value={formData.fatherName} onChange={handleChange} />
+            <label>Date of Birth (DD/MM/YY):</label>
+            <input type="text" name="dob" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label>Gender:</label>
+            <select name="gender" className="form-control">
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="Transgender">Transgender</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Father's/Husband's Name:</label>
+            <input type="text" name="fatherHusbandName" className="form-control" />
           </div>
           <div className="form-group">
             <label>Mother's Name:</label>
-            <input type="text" name="motherName" value={formData.motherName} onChange={handleChange} />
+            <input type="text" name="motherName" className="form-control" />
           </div>
-        </div>
-
-        <div className="row">
           <div className="form-group">
             <label>Father's Occupation:</label>
-            <input type="text" name="occupation" value={formData.occupation} onChange={handleChange} />
+            <input type="text" name="occupation" className="form-control" />
           </div>
           <div className="form-group">
             <label>Monthly Income:</label>
-            <input type="text" name="monthlyIncome" value={formData.monthlyIncome} onChange={handleChange} />
+            <input type="number" name="monthlyIncome" className="form-control" />
           </div>
-        </div>
-
-        <div className="form-group">
-          <label>Postal Address:</label>
-          <input type="text" name="postalAddress" value={formData.postalAddress} onChange={handleChange} />
-        </div>
-
-        <div className="form-group">
-          <label>Permanent Address:</label>
-          <input type="text" name="permanentAddress" value={formData.permanentAddress} onChange={handleChange} />
-        </div>
-
-        <div className="form-group">
-          <label>Phone No:</label>
-          <input type="text" name="phoneNo" value={formData.phoneNo} onChange={handleChange} />
-        </div>
-
-        <div className="row">
+          <div className="form-group">
+            <label>Postal Address:</label>
+            <textarea rows="3" name="postalAddress" className="form-control"></textarea>
+          </div>
+          <div className="form-group">
+            <label>Permanent Address:</label>
+            <textarea rows="3" name="permanentAddress" className="form-control"></textarea>
+          </div>
+          <div className="form-group">
+            <label>Phone No:</label>
+            <input type="number" name="phoneNo" className="form-control" />
+          </div>
           <div className="form-group">
             <label>Nationality:</label>
-            <input type="text" name="nationality" value={formData.nationality} onChange={handleChange} />
+            <input type="text" name="nationality" className="form-control" />
           </div>
+        </fieldset>
+        <fieldset>
+          <legend><b>EDUCATION (LAST EXAM)</b></legend>
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th>EXAM PASSED</th>
+                <th>BOARD/UNIVERSITY</th>
+                <th>YEAR OF PASSING</th>
+                <th>DIVISION</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td><input type="text" name="examPassed" className="form-control" /></td>
+                <td><input type="text" name="boardUniversity" className="form-control" /></td>
+                <td><input type="text" name="yearOfPassing" className="form-control" /></td>
+                <td><input type="text" name="division" className="form-control" /></td>
+              </tr>
+              <tr>
+                <td><input type="text" name="examPassed" className="form-control" /></td>
+                <td><input type="text" name="boardUniversity" className="form-control" /></td>
+                <td><input type="text" name="yearOfPassing" className="form-control" /></td>
+                <td><input type="text" name="division" className="form-control" /></td>
+              </tr>
+            </tbody>
+          </table>
+        </fieldset>
+        <fieldset>
+          <legend><b>DECLARATION</b></legend>
           <div className="form-group">
-            <label>Marital Status:</label>
-            <input type="text" name="maritalStatus" value={formData.maritalStatus} onChange={handleChange} />
+            <input type="checkbox" name="declaration" />
+            <label>
+              I hereby declare that all the statements in the application are true to the best of my knowledge and belief.
+            </label>
           </div>
+        </fieldset>
+        <div className="text-center">
+          <button type="submit" className="btn btn-primary" onClick={() => alert('Submitted Successfully')}>Submit</button>
         </div>
-
-        <h3>Education (Last Exam Passed)</h3>
-        <div className="row">
-          <div className="form-group">
-            <label>Exam Passed:</label>
-            <input type="text" name="examPassed" value={formData.examPassed} onChange={handleChange} />
-          </div>
-          <div className="form-group">
-            <label>Board/University:</label>
-            <input type="text" name="boardUniversity" value={formData.boardUniversity} onChange={handleChange} />
-          </div>
-          <div className="form-group">
-            <label>Year of Passing:</label>
-            <input type="text" name="yearOfPassing" value={formData.yearOfPassing} onChange={handleChange} />
-          </div>
-          <div className="form-group">
-            <label>Division:</label>
-            <input type="text" name="division" value={formData.division} onChange={handleChange} />
-          </div>
-        </div>
-
-        <div className="declaration">
-          <p>I hereby declare that all statements in the application are true to the best of my knowledge and belief.</p>
-          <div className="row">
-            <div className="form-group">
-              <label>Date:</label>
-              <input type="date" name="date" value={formData.date} onChange={handleChange} />
-            </div>
-            <div className="form-group">
-              <label>Place:</label>
-              <input type="text" name="place" value={formData.place} onChange={handleChange} />
-            </div>
-          </div>
-          <div className="row">
-            <div className="form-group">
-              <label>Signature of Parents/Guardians:</label>
-              <input type="text" name="guardianSignature" value={formData.guardianSignature} onChange={handleChange} />
-            </div>
-            <div className="form-group">
-              <label>Signature of Applicant:</label>
-              <input type="text" name="applicantSignature" value={formData.applicantSignature} onChange={handleChange} />
-            </div>
-          </div>
-        </div>
-
-        <button type="submit" className="submit-btn">Submit</button>
       </form>
     </div>
   );
